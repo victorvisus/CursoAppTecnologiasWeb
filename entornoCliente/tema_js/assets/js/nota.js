4f25f77fd195f4a4c87a5d392226e4ccb8f5e931
victorvisus <victor.vxg@gmail.com>
2026-02-26 12:32:25 +0100

    end condicionales

---
+++ entornoCliente/tema_js/assets/js/nota.js
@@ -0,0 +1,72 @@
+/**
+ *     <ul>Convierte la nota a letra según:
+<li>90-100: A</li>
+<li>80-89: B</li>
+<li>70-79: C</li>
+<li>60-69: D</li>
+<li>0-59: F</li>
+*/
+let calif;
+
+/**
+ * @param {number} dato
+ */
+function notaSwitch(dato) {
+  switch (true) {
+    case dato >= 90 && dato <= 100:
+      calif = 'A';
+      break;
+    case dato >= 80 && dato <= 89:
+      calif = 'B';
+      break;
+    case dato >= 70 && dato <= 79:
+      calif = 'C';
+      break;
+    case dato >= 60 && dato <= 69:
+      calif = 'D';
+      break;
+    default:
+      calif = 'F';
+  }
+  alert(`Tu calificacion es ${calif}`);
+}
+
+/**
+ *
+ * @param {number} dato
+ */
+function notaTernario(dato) {
+  calif =
+    dato >= 90 && dato <= 100
+      ? 'A'
+      : dato >= 80 && dato <= 89
+        ? 'B'
+        : dato >= 70 && dato <= 79
+          ? 'C'
+          : dato >= 60 && dato <= 69
+            ? 'D'
+            : 'F';
+
+  alert(`Tu calificacion es ${calif}`);
+}
+
+/**
+ *
+ * @returns {number}
+ */
+function solicitarDato() {
+  let dato = parseInt(prompt('¿Cual es la nota numérica (0-100)? '));
+  alert(`El valor introducido es: ${dato}.`);
+
+  /*   while (isNaN(dato) && dato <= 0 && dato >= 100) {
+    if (isNaN(dato)) {
+      dato = parseInt(prompt('Introduce un valor númerico entre 0 y 100'));
+    } else if (dato <= 0 && dato >= 100) {
+      dato = parseInt(
+        prompt('La nota debe ser entre 0 y 100, introduce un valor correcto'),
+      );
+    }
+  } */
+  // Devuelve la edad del usuario
+  return parseInt(dato);
+}
