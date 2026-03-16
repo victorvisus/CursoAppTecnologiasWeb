21ac8ba76b812aef1301156b455993518606cff1
victorvisus <victor.vxg@gmail.com>
2026-03-16 14:30:18 +0100

    tarea

---
+++ entornoCliente/03_js/assets/js/spread.js
@@ -0,0 +1,5 @@
+/* Para una agenda: se requiere un objeto que sea jornada -> entre otros horario, clientes...
+habrá dias que se queden sin asignación horaria, y otros que se complete -> destructuring en un sentido y spread en el sentido opuesto.
+si se completa el objeto semna, habrá semanas con dias festivos y otras que no: destructuring y spread */
+// se pueden guardar datos en ficheros externos .txt
+//mirar tambien window.localStorage
