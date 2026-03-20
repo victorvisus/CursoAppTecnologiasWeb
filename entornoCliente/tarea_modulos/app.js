e7de1528b2eb1e320b83f50ea511e324fb723971
victorvisus <victor.vxg@gmail.com>
2026-03-20 14:14:27 +0100

    Tarea imports y exports

---
+++ entornoCliente/tarea_modulos/app.js
@@ -0,0 +1,21 @@
+/*
+•Importa todo lo necesario
+•Crea algunos productos
+•Agrégalos al gestor
+•Muestra la lista y el total
+*/
+import GestorProducto from './src/gestor/GestorProducto.js';
+import Producto from './src/modelos/Producto.js';
+
+const miGestor = new GestorProducto();
+
+const p1 = new Producto('p1', 10, 5);
+const p2 = new Producto('p2', 20, 10);
+const p3 = new Producto('p3', 30, 15);
+
+miGestor.agregar(p1);
+miGestor.agregar(p2);
+miGestor.agregar(p3);
+
+console.log(miGestor.listar());
+console.log(miGestor.obtenerTotal());
---
+++ entornoCliente/tarea_modulos/src/gestor/GestorProducto.js
@@ -0,0 +1,59 @@
+/*
+•Exporta clase GestorProducto
+•Propiedad: array de productos
+•Método: agregar(producto) - valida antes de agregar
+•Método: listar() - devuelve strings formateados
+•Método: obtenerTotal() - suma de precio * stock de todos
+*/
+import { formatearPrecio, formatearStock } from '../utils/formateo.js';
+import { validarPrecio, validarStock } from '../utils/validaciones.js';
+
+export default class GestorProducto {
+  constructor() {
+    this.productos = [];
+  }
+  agregar(_producto) {
+    if (!validarPrecio(_producto.precio) || !validarStock(_producto.stock))
+      throw new Error('Producto no valido');
+    this.productos.push(_producto);
+  }
+  listar() {
+    let listado = '';
+    console.log('LISTANDO PRODUCTOS');
+    for (let i = 0; i < this.productos.length; i++) {
+      listado = listado.concat(
+        '\nProducto: ' +
+          this.productos[i].nombre +
+          ' -------------------' +
+          '\n· Precio: ' +
+          formatearPrecio(this.productos[i].precio) +
+          '\n· Stock: ' +
+          formatearStock(this.productos[i].stock) +
+          ' \n',
+      );
+    }
+    return listado;
+  }
+  obtenerTotal() {
+    let total = 0;
+    for (let i = 0; i < this.productos.length; i++) {
+      total += this.productos[i].precio * this.productos[i].stock;
+    }
+    return total;
+  }
+}
+
+/*
+
+    agregar(_producto) {
+        if (validarPrecio(_producto.precio) && validarStock(_producto.stock)) {
+            this.productos.push(_producto);
+        }
+    }
+    listar() {
+        return this.productos.map(producto => producto.toString());
+    }
+    obtenerTotal() {
+        return this.productos.reduce((total, producto) => total + producto.precio * producto.stock, 0);
+    }
+*/
