8f50ba81ba8f55febe3a4adb4eb4814081d37afe
victorvisus <victor.vxg@gmail.com>
2026-03-20 14:25:58 +0100

    poner try catch en app.js de tarea modules

--- entornoCliente/tarea_modulos/app.js
+++ entornoCliente/tarea_modulos/app.js
@@ -7,15 +7,19 @@
 import GestorProducto from './src/gestor/GestorProducto.js';
 import Producto from './src/modelos/Producto.js';
 
-const miGestor = new GestorProducto();
+try {
+  const miGestor = new GestorProducto();
 
-const p1 = new Producto('p1', 10, 5);
-const p2 = new Producto('p2', 20, 10);
-const p3 = new Producto('p3', 30, 15);
+  const p1 = new Producto('p1', 10, 5);
+  const p2 = new Producto('p2', 20, 10);
+  const p3 = new Producto('p3', 30, 15);
 
-miGestor.agregar(p1);
-miGestor.agregar(p2);
-miGestor.agregar(p3);
+  miGestor.agregar(p1);
+  miGestor.agregar(p2);
+  miGestor.agregar(p3);
 
-console.log(miGestor.listar());
-console.log(miGestor.obtenerTotal());
+  console.log(miGestor.listar());
+  console.log(miGestor.obtenerTotal());
+} catch (error) {
+  console.log(`ERROR: ` + error.message + '\nFin de la ejecución');
+}
